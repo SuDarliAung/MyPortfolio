35dfb7dba72b7d34302feda86bbb05e157803ec7
Su Darli Aung <sudarli.aung@partnerassociates.com>
2024-09-23 17:40:10 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,156 @@
+// htmlcss progress circular bar 
+/*!
+    Title: Dev Portfolio Template
+    Version: 1.2.2
+    Last Change: 03/25/2020
+    Author: Ryan Fitzgerald
+    Repo: https://github.com/RyanFitzgerald/devportfolio-template
+    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
+
+    Description: This file contains all the scripts associated with the single-page
+    portfolio website.
+*/
+!function(n){n("html").removeClass("no-js"),n("header a").click(function(e){if(!n(this).hasClass("no-scroll")){e.preventDefault();var t=n(this).attr("href"),i=n(t).offset().top;n("html, body").animate({scrollTop:i+"px"},+Math.abs(window.pageYOffset-n(t).offset().top)),n("header").hasClass("active")&&n("header, body").removeClass("active")}}),n("#to-top").click(function(){n("html, body").animate({scrollTop:0},500)}),n("#lead-down span").click(function(){var e=n("#lead").next().offset().top;n("html, body").animate({scrollTop:e+"px"},500)}),n("#experience-timeline").each(function(){$this=n(this),$userContent=$this.children("div"),$userContent.each(function(){n(this).addClass("vtimeline-content").wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>')}),$this.find(".vtimeline-point").each(function(){n(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>')}),$this.find(".vtimeline-content").each(function(){var e=n(this).data("date");e&&n(this).parent().prepend('<span class="vtimeline-date">'+e+"</span>")})}),n("#mobile-menu-open").click(function(){n("header, body").addClass("active")}),n("#mobile-menu-close").click(function(){n("header, body").removeClass("active")}),n("#view-more-projects").click(function(e){e.preventDefault(),n(this).fadeOut(300,function(){n("#more-projects").fadeIn(300)})})}(jQuery);
+let htmlProgress = document.querySelector(".html-css"),
+  htmlValue = document.querySelector(".html-progress");
+
+let htmlStartValue = 0,
+  htmlEndValue = 90,
+  htmlspeed = 30;
+
+let progresshtml = setInterval(() => {
+  htmlStartValue++;
+
+  htmlValue.textContent = `${htmlStartValue}%`;
+  htmlProgress.style.background = `conic-gradient(#fca61f ${
+    htmlStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (htmlStartValue == htmlEndValue) {
+    clearInterval(progresshtml);
+  }
+}, htmlspeed);
+
+// javasript progress circular bar 
+let javascriptProgress = document.querySelector(".javascript"),
+  javascriptValue = document.querySelector(".javascript-progress");
+
+let javascriptStartValue = 0,
+  javascriptEndValue = 75,
+  jsspeed = 30;
+
+let progressjs = setInterval(() => {
+  javascriptStartValue++;
+
+  javascriptValue.textContent = `${javascriptStartValue}%`;
+  javascriptProgress.style.background = `conic-gradient(#7d2ae8 ${
+    javascriptStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (javascriptStartValue == javascriptEndValue) {
+    clearInterval(progressjs);
+  }
+}, jsspeed);
+
+// php progress circular bar 
+let phpProgress = document.querySelector(".php"),
+  phpValue = document.querySelector(".php-progress");
+
+let phpStartValue = 0,
+  phpEndValue = 80,
+  phpspeed = 30;
+
+let progressphp = setInterval(() => {
+  phpStartValue++;
+
+  phpValue.textContent = `${phpStartValue}%`;
+  phpProgress.style.background = `conic-gradient(#20c997 ${
+    phpStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (phpStartValue == phpEndValue) {
+    clearInterval(progressphp);
+  }
+}, phpspeed);
+
+// reactjs progress circular bar 
+let reactProgress = document.querySelector(".reactjs"),
+  reactValue = document.querySelector(".reactjs-progress");
+
+let reactStartValue = 0,
+  reactEndValue = 30,
+  rjsspeed = 30;
+
+let progressreact = setInterval(() => {
+  reactStartValue++;
+
+  reactValue.textContent = `${reactStartValue}%`;
+  reactProgress.style.background = `conic-gradient(#3f396d ${
+    reactStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (reactStartValue == reactEndValue) {
+    clearInterval(progressreact);
+  }
+}, rjsspeed);
+
+
+// filter using javascript
+$(document).ready(function () {
+  $(".filter-item").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".post").show("1000");
+    } else {
+      $(".post")
+        .not("." + value)
+        .hide("1000");
+      $(".post")
+        .filter("." + value)
+        .show("1000");
+    }
+  });
+});
+
+
+// javascript for sticky navbar even if u scroll the navbar will be fixed
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 50) {
+        document.getElementById('navbar-top').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar-top').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+  });
+}); 
+
+
+// adding funtionality to back to top button 
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click",function(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
